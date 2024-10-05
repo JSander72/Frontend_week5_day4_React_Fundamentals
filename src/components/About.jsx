55bf49130539501802d7392d1caf4f8a7e328ee0
JSander72 <jamesearlsanders2@outlook.com>
2024-10-05 09:56:40 -0400

    update 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,17 @@
 import React from "react";
 
 const About = () => {
-    return (
-      <section>
-        <h2>About Me</h2>
-        <p>
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
         Raised in Birmingham, Alabama, and a graduate of Miles College.
-                    Throughout my life, I've embraced challenges and overcome them with
-                    determination. I'm excited to leverage this same resilience as I
-                    pursue a rewarding career in software engineering.
-        </p>
-      </section>
-    );
-  }
-
-
+        Throughout my life, I've embraced challenges and overcome them with
+        determination. I'm excited to leverage this same resilience as I
+        pursue a rewarding career in software engineering.
+      </p>
+    </section>
+  );
+};
 
 export default About;
